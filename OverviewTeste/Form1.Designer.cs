0d2166aaa2311bb5225433b7b4aca77fd4536bbe
Mácio Júnior <juniorlimafl@gmail.com>
2022-12-26 14:05:26 -0300

    Inclusão de funções do CRUD.

--- OverviewTeste/Form1.Designer.cs
+++ OverviewTeste/Form1.Designer.cs
@@ -28,21 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
             this.btnAdicionar = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.UserName = new System.Windows.Forms.ColumnHeader();
+            this.DataNasc = new System.Windows.Forms.ColumnHeader();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.alterarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deletarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(12, 58);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(776, 341);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // label1
             // 
@@ -59,6 +58,7 @@
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(488, 23);
             this.txtNome.TabIndex = 2;
+            //this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
             // 
             // label2
             // 
@@ -77,6 +77,7 @@
             this.dtpNascimento.Name = "dtpNascimento";
             this.dtpNascimento.Size = new System.Drawing.Size(98, 23);
             this.dtpNascimento.TabIndex = 4;
+            //this.dtpNascimento.ValueChanged += new System.EventHandler(this.dtpNascimento_ValueChanged);
             // 
             // btnAdicionar
             // 
@@ -86,32 +87,88 @@
             this.btnAdicionar.TabIndex = 5;
             this.btnAdicionar.Text = "Adicionar";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.UserName,
+            this.DataNasc});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 71);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 316);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            //this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // UserName
+            // 
+            this.UserName.Text = "NOME";
+            this.UserName.Width = 490;
+            // 
+            // DataNasc
+            // 
+            this.DataNasc.Text = "DATA DE NASCIMENTO";
+            this.DataNasc.Width = 176;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alterarToolStripMenuItem,
+            this.deletarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(112, 48);
+            this.contextMenuStrip1.Text = "Opções";
+            //this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // alterarToolStripMenuItem
+            // 
+            this.alterarToolStripMenuItem.Name = "alterarToolStripMenuItem";
+            this.alterarToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.alterarToolStripMenuItem.Text = "Alterar";
+            this.alterarToolStripMenuItem.Click += new System.EventHandler(this.alterarToolStripMenuItem_Click);
+            // 
+            // deletarToolStripMenuItem
+            // 
+            this.deletarToolStripMenuItem.Name = "deletarToolStripMenuItem";
+            this.deletarToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.deletarToolStripMenuItem.Text = "Deletar";
+            this.deletarToolStripMenuItem.Click += new System.EventHandler(this.deletarToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 411);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.dtpNascimento);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Cadastro Usuarios";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private ListView listView1;
         private Label label1;
         private TextBox txtNome;
         private Label label2;
         private DateTimePicker dtpNascimento;
         private Button btnAdicionar;
+        private ListView listView1;
+        private ColumnHeader UserName;
+        private ColumnHeader DataNasc;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem alterarToolStripMenuItem;
+        private ToolStripMenuItem deletarToolStripMenuItem;
     }
 }
--- OverviewTeste/Form1.cs
+++ OverviewTeste/Form1.cs
@@ -6,5 +6,66 @@ namespace OverviewTeste
         {
             InitializeComponent();
         }
+
+
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // Somente letras maiúsculas
+            txtNome.Text = txtNome.Text.ToUpper();
+
+            // Não permite campo vazio
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Não é permitido adicionar campo vazio!");
+                return;
+            }
+            // Add nome e data de nascimento
+            ListViewItem item = new ListViewItem(txtNome.Text);
+            MessageBox.Show("Usuário cadastrado com sucesso!");
+
+            item.SubItems.Add(dtpNascimento.Text);
+            listView1.Items.Add(item);
+            // limpar campo após adicionar nome na lista
+            txtNome.Clear();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+   
+
+        private void deletarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+            {
+                MessageBox.Show("O REGISTRO FOI EXCLUÍDO COM SUCESSO!");
+                return;
+            }
+        }
+        private void alterarToolStripMenuItem_Click(object sender, EventArgs e)
+
+        { if (listView1.SelectedIndices.Count > 0)
+            {
+                int selectedIndex = listView1.SelectedIndices[0];
+
+                if (txtNome.Text != "")
+                {
+                    listView1.Items[selectedIndex].SubItems[0].Text = txtNome.Text.ToUpper();
+                    listView1.Items[selectedIndex].SubItems[1].Text = dtpNascimento.Text;
+
+                    txtNome.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Não é permitido alterar um campo vazio!");
+                    return;
+                }                  
+            }
+             
+        }
     }
 }
